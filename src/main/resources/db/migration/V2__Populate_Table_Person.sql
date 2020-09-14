115884aa8c69e571e7d80a1561a733595cf05a51
Marcos Calicy <mvcalicy@gmail.com>
2020-09-13 22:54:49 -0300

    Adicionando uso do verbo PATCH

--- src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,4 +1,4 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gendere`, `last_name`) VALUES
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
 	(1, 'Uberlândia - Minas Gerais - Brasil', 'Leandro', 'Male', 'Costa'),
 	(2, 'Uberlândia - Minas Gerais - Brasil', 'Gabriela', 'Female', 'Costa'),
 	(5, 'Patos de Minas - Minas Gerais - Brasil', 'Flávio', 'Male', 'Costa'),
